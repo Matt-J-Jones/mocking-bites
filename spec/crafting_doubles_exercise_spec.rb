8ccdc51594901759c088d82acdf8b130310a807e
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-03-20 17:14:52 +0000

    Complete - Crafting Doubles Exercise

--- spec/crafting_doubles_exercise_spec.rb
+++ spec/crafting_doubles_exercise_spec.rb
@@ -1,15 +1,15 @@
 RSpec.describe "doubles" do
-  xit "sets up a blank double" do
+  it "sets up a blank double" do
     # Set up your doubles here
-    fake_object = # ...
+    fake_object = double()
 
     # Don't edit below
     expect(fake_object).to be
   end
 
-  xit "sets up a double with methods" do
+  it "sets up a double with methods" do
     # Set up your doubles here
-    fake_object = # ...
+    fake_object = double(:fake_object, speak: "Meow", count_ears: 2, count_legs: 4)
 
     # Don't edit below
     expect(fake_object.speak).to eq "Meow"
@@ -17,9 +17,10 @@ RSpec.describe "doubles" do
     expect(fake_object.count_legs).to eq 4
   end
 
-  xit "sets up a double with methods that take arguments" do
+  it "sets up a double with methods that take arguments" do
     # Set up your doubles here
-    fake_object = # ...
+    fake_object = double(:fake_object)
+    allow(fake_object).to receive(:speak).with("Jess").and_return("Meow, Jess")
 
     # Don't edit below
     expect(fake_object.speak("Jess")).to eq "Meow, Jess"
@@ -28,19 +29,19 @@ RSpec.describe "doubles" do
     expect { fake_object.speak("Ron") }.to raise_error RSpec::Mocks::MockExpectationError
   end
 
-  xit "sets up doubles that expect to be called" do
+  it "sets up doubles that expect to be called" do
     fake_object = double :animal
     # Write an expectation below that the method "speak" is called with
     # the argument "Steve"
-
+    allow(fake_object).to receive(:speak).with("Steve").and_return("Steve")
     # ...
 
     # Don't edit below
     fake_object.speak("Steve")
   end
 
-  xit "creates a double for a specific case" do
-    fake_diary = double :diary, add: nil
+  it "creates a double for a specific case" do
+    fake_diary = double :diary, add: nil, count_entries: 2
     # Set up this double to pass the tests below
     # ...
 
