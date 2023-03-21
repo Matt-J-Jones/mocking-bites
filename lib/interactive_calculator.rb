b07b842159c1bbab3c4d0cccf4d10b93dc22f25c
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-03-21 17:07:10 +0000

    Complete Unit Testing Terminal IO Exercise

--- lib/interactive_calculator.rb
+++ lib/interactive_calculator.rb
@@ -4,14 +4,14 @@ class InteractiveCalculator
   end
 
   def run
-    puts "Hello. I will subtract two numbers."
-    puts "Please enter a number"
-    number_0 = gets.chomp
-    puts "Please enter another number"
-    number_1 = gets.chomp
-    puts "Here is your result:"
-    result = number_0.to_i-number_1.to_i
-    puts "#{number_0} - #{number_1} = #{result}"
+    @io.puts "Hello. I will subtract two numbers."
+    @io.puts "Please enter a number"
+    number_0 = @io.gets.chomp
+    @io.puts "Please enter another number"
+    number_1 = @io.gets.chomp
+    @io.puts "Here is your result:"
+    result = number_0.to_i - number_1.to_i
+    @io.puts "#{number_0} - #{number_1} = #{result}"
   end
 end
 
