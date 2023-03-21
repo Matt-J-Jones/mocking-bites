06149e1a77686c0a7fe55b90a674a2f63f8eb16a
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-03-21 16:47:34 +0000

    Complete Unit Testing API Requests - Challenge

--- spec/cat_facts_spec.rb
+++ spec/cat_facts_spec.rb
@@ -1,1 +1,14 @@
 require 'cat_facts'
+
+RSpec.describe CatFacts do
+  it "shows cat fact" do
+    fake_responder = double :responder
+    allow(fake_responder).to receive(:get).with(
+      URI("https://catfact.ninja/fact")
+    ).and_return('{"fact":"Kittens remain with their mother till the age of 9 weeks.","length":57}')
+  
+    cat_fact = CatFacts.new(fake_responder)
+    result = cat_fact.provide
+    expect(result).to eq "Cat fact: Kittens remain with their mother till the age of 9 weeks."
+  end
+end
