0538e01a42dad8edaa70d2e70ef964d341b85a82
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-03-22 10:15:14 +0000

    Complete Unit Testing Terminal IO Challenge

--- lib/string_repeater.rb
+++ lib/string_repeater.rb
@@ -1,10 +1,19 @@
-string_repeater = StringRepeater.new(Kernel)
-string_repeater.run
+class StringRepeater
+  def initialize(io = Kernel)
+    @io = io
+  end
 
-# Hello. I will repeat a string many times.
-# Please enter a string
-# TWIX
-# Please enter a number of repeats
-# 10
-# Here is your result:
-# TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX
+  def run
+    @io.puts "Hello. I will repeat a string many times."
+    @io.puts "Please enter a string"
+    string = @io.gets.chomp
+    @io.puts "Please enter a number of repeats"
+    repeat = @io.gets.chomp.to_i
+    result = string * repeat
+    @io.puts "Here is your result:"
+    @io.puts result
+  end
+end
+
+# string_repeater = StringRepeater.new()
+# string_repeater.run
--- spec/string_repeater_spec.rb
+++ spec/string_repeater_spec.rb
@@ -1,1 +1,19 @@
 require 'string_repeater'
+
+RSpec.describe StringRepeater do
+  it "returns repeated string" do
+    io = double :io
+
+    expect(io).to receive(:puts).with("Hello. I will repeat a string many times.")
+    expect(io).to receive(:puts).with("Please enter a string")
+    expect(io).to receive(:gets).and_return("TWIX")
+    expect(io).to receive(:puts).with("Please enter a number of repeats")
+    expect(io).to receive(:gets).and_return("10")
+    expect(io).to receive(:puts).with("Here is your result:")
+    expect(io).to receive(:puts).with("TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX")
+
+    repeater = StringRepeater.new(io)
+    repeater.run
+
+  end
+end
